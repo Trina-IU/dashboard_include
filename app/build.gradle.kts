cae506e9bbdab833a0676262b51065e51ca5d4fb
VengeTH <prinzemikhailsadsad@gmail.com>
2025-03-01 23:02:35 +0800

    added the implementation for tesstwo and camerax. will setup the camera preview on the camera via camerax.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,13 @@ android {
 }
 
 dependencies {
+    //TessTwo OCR(Handwriting recognition)
+    implementation("com.rmtheis:tess-two:9.1.0")
+    //CameraX for camera integration
+    implementation ("androidx.camera:camera-core:1.2.3")
+    implementation ("androidx.camera:camera-camera2:1.2.3")
+    implementation ("androidx.camera:camera-lifecycle:1.2.3")
+    implementation ("androidx.camera:camera-view:1.2.3")
     implementation(libs.firebase.auth)
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-auth")
