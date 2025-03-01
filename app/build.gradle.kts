7d537d0c369389bdbfa8fab45b5db5d6d9744eb2
VengeTH <prinzemikhailsadsad@gmail.com>
2025-03-02 00:26:20 +0800

    fixed gradle shits and upgraded the firebase version into the latest version. downgraded the gradle version into 8.7.3 from 8.8.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
     id("com.google.gms.google-services")
 }
 
@@ -51,7 +51,7 @@ dependencies {
     implementation ("androidx.camera:camera-lifecycle:1.2.3")
     implementation ("androidx.camera:camera-view:1.2.3")
     implementation(libs.firebase.auth)
-    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-firestore")
