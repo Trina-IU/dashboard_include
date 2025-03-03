e5cb41d89ccaf6ba4b66f4a1303e7f6ca895f82e
Trina-IU <trinamarbellaofficial@gmail.com>
2025-03-03 20:36:53 +0800

    added terms and conditions xml

--- app/src/main/java/com/example/med_sample/TermsconditionActivity.java
+++ app/src/main/java/com/example/med_sample/TermsconditionActivity.java
@@ -1,4 +1,14 @@
 package com.example.med_sample;
 
-public class TermsconditionActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TermsconditionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_termscondition);
+    }
 }
+
