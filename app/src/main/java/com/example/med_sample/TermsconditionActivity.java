c6956aca25dfb83ff46c63053a289cb692ea6ea7
VengeTH <prinzemikhailsadsad@gmail.com>
2025-03-03 20:45:05 +0800

    terms and conditions accept button will go into login page

--- app/src/main/java/com/example/med_sample/TermsconditionActivity.java
+++ app/src/main/java/com/example/med_sample/TermsconditionActivity.java
@@ -1,6 +1,8 @@
 package com.example.med_sample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +11,11 @@ public class TermsconditionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_termscondition);
+        Button accept = (Button) findViewById(R.id.accept_btn);
+        accept.setOnClickListener(v -> {;
+            Intent intent = (new Intent(TermsconditionActivity.this, LoginActivity.class));
+            startActivity(intent);
+        });
     }
 }
 
